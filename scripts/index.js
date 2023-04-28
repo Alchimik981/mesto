7db6c614431caf7a30d3d20092927a332b0d5830
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-04-28 22:36:27 +0300

    fix, html,css,js

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,20 @@ let editButton = profile.querySelector('.profile__edit-btn')
 let addButton = profile.querySelector('.profile__add-btn');
 let popup = document.querySelector('.popup');
 let closePopup = popup.querySelector('.popup__close');
-
-editButton.addEventListener('click', () => popup.classList.add('popup_active'));
-
-closePopup.addEventListener('click', () => popup.classList.remove('popup_active'));
-
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.input__text_type_name');
 let aboutInput = document.querySelector('.input__text_type_about');
+let savePopup = document.querySelector('.popup__save-btn')
+
+editButton.addEventListener('click', () => popup.classList.add('popup_active'));
+//closePopup.addEventListener('click', () => popup.classList.remove('popup_active'));
+function closeEditPopup() {
+  popup.classList.remove('popup_active');
+}
+
+closePopup.addEventListener('click', closeEditPopup);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -18,15 +24,13 @@ function handleFormSubmit(evt) {
   nameInput = document.getElementById('input_name').value;
   aboutInput = document.getElementById('input_about').value;
 
-  let profileName = document.querySelector('.profile__name');
-  let profileAbout = document.querySelector('.profile__about');
-
   profileName.textContent = `${nameInput}`;
   profileAbout.textContent = `${aboutInput}`;
 
-  popup.classList.remove('popup_active');
+  closeEditPopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
 
 

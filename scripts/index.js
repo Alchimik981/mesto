13ae56450fbfe49ae0f23880b88a7fd4a1be8fc4
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-05-12 20:22:36 +0300

    2 buttons

--- scripts/index.js
+++ scripts/index.js
@@ -8,17 +8,46 @@ let profileAbout = document.querySelector('.profile__about');
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.input__text_type_name');
 let aboutInput = document.querySelector('.input__text_type_about');
-let savePopup = document.querySelector('.popup__save-btn')
+let savePopup = document.querySelector('.popup__save-btn');
+let elementText = document.querySelector('.element__text');
+let elementPic = document.querySelector('.element__pic');
+let popupTitle = popup.querySelector('.popup__title');
 
 function popupClose() {
   popup.classList.remove('popup_active');
 }
 
-editButton.addEventListener('click', () => popup.classList.add('popup_active'));
+function popupOpen() {
+  popup.classList.add('popup_active');
+}
+
+addButton.addEventListener('click', openAddPopup);
+editButton.addEventListener('click', openEditPopup);
 closePopup.addEventListener('click', popupClose);
 
+function openEditPopup() {
+  popupOpen()
+  popupTitle.innerText = 'Редактировать профиль';
+  nameInput = popup.querySelector('.input-text_type_name').value = "Жак-Ив Кусто";
+  nameInput = popup.querySelector('.input-text_type_name').placeholder = 'Введите имя';
+  aboutInput = popup.querySelector('.input-text_type_about').value = "Исследователь океана";
+  aboutInput = popup.querySelector('.input-text_type_about').placeholder = 'О себе';
+  formElement.addEventListener('submit', editFormSubmit);
+}
 
-function handleFormSubmit(evt) {
+
+function openAddPopup() {
+  popupOpen();
+  popupTitle.innerText = 'Новое место';
+  nameInput = popup.querySelector('.input-text_type_name').value = "";
+  nameInput = popup.querySelector('.input-text_type_name').placeholder = 'Название';
+  aboutInput = popup.querySelector('.input-text_type_about').value = "";
+  aboutInput = popup.querySelector('.input-text_type_about').placeholder = 'Добавьте ссылку';
+  
+  formElement.addEventListener('submit', addFormSubmit);
+}
+
+function editFormSubmit(evt) {
   evt.preventDefault();
 
   nameInput = document.querySelector('.input-text_type_name').value;
@@ -26,11 +55,21 @@ function handleFormSubmit(evt) {
 
   profileName.textContent = `${nameInput}`;
   profileAbout.textContent = `${aboutInput}`;
-
   popupClose();
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+function addFormSubmit(evt) {
+  evt.preventDefault();
+
+  nameInput = document.querySelector('.input-text_type_name').value;
+  aboutInput = document.querySelector('.input-text_type_about').value;
+
+  elementText.textContent = `${nameInput}`;
+  elementPic = document.querySelector('.element__pic').src = `${aboutInput}`;
+  popupClose();
+}
+
+
 
 
 

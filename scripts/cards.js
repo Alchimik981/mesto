abbba5de98794c0a4ab6d153fe811cb0295daf04
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-05-18 18:18:43 +0300

    fix rewiev

---
+++ scripts/cards.js
@@ -0,0 +1,34 @@
+let initialCards = [
+  {
+    name: "Таганрог",
+    link: "https://images.unsplash.com/photo-1568924509837-7cb0751c0d84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Краснодар",
+    link: "https://images.unsplash.com/photo-1547135288-962281f3efe6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  },
+  {
+    name: "Сибирь",
+    link: "https://images.unsplash.com/photo-1630475915483-7e492dcccf18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Санкт-Петербург",
+    link: "https://images.unsplash.com/photo-1615529489302-e5e8d9f72ce8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Алтай",
+    link: "https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=867&q=80",
+  },
+  {
+    name: "Роза Хутор",
+    link: "https://images.unsplash.com/photo-1617117833203-c91b04e0431f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+  {
+    name: "Байкал",
+    link: "https://images.unsplash.com/photo-1552735855-557bdba3961a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80",
+  },
+];
